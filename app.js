0d3646cba02d13580243583526d72d8e23f6d628
andrije <gleb.andrijevski@gmail.com>
2026-04-16 23:05:24 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const tableElement = document.getElementById('table')
+const removeBtn = document.getElementById('remove')
 
 const classes = [
   {
@@ -7,6 +8,7 @@ const classes = [
     time: '9:00 AM - 11:00 AM',
     instructor: 'Aaron Chapman',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Mind & Body',
@@ -14,6 +16,7 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Adam Stewart',
     capacity: 15,
+    settings: [false, '+'],
   },
   {
     title: 'Crit Cardio',
@@ -21,6 +24,7 @@ const classes = [
     time: '9:00 AM - 10:00 AM',
     instructor: 'Aaron Chapman',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Wheel Pose Full Posture',
@@ -28,6 +32,7 @@ const classes = [
     time: '7:00 AM - 8:30 AM',
     instructor: 'Donna Wilson',
     capacity: 15,
+    settings: [false, '+'],
   },
   {
     title: "Playful Dancer's Flow",
@@ -35,6 +40,7 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Donna Wilson',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Zumba Dance',
@@ -42,6 +48,7 @@ const classes = [
     time: '5:00 PM - 7:00 PM',
     instructor: 'Donna Wilson',
     capacity: 20,
+    settings: [false, '+'],
   },
   {
     title: 'Cardio Blast',
@@ -49,6 +56,7 @@ const classes = [
     time: '5:00 PM - 7:00 PM',
     instructor: 'Randy Porter',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Pilates Reformer',
@@ -56,6 +64,7 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Randy Porter',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Supple Spine and Shoulders',
@@ -63,6 +72,7 @@ const classes = [
     time: '6:30 AM - 8:00 AM',
     instructor: 'Randy Porter',
     capacity: 15,
+    settings: [false, '+'],
   },
   {
     title: 'Yoga for Divas',
@@ -70,6 +80,7 @@ const classes = [
     time: '9:00 AM - 11:00 AM',
     instructor: 'Donna Wilson',
     capacity: 20,
+    settings: [false, '+'],
   },
   {
     title: 'Virtual Cycle',
@@ -77,6 +88,7 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Randy Porter',
     capacity: 20,
+    settings: [false, '+'],
   },
 
   // repeated entries
@@ -86,6 +98,7 @@ const classes = [
     time: '9:00 AM - 11:00 AM',
     instructor: 'Aaron Chapman',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Mind & Body',
@@ -93,6 +106,7 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Adam Stewart',
     capacity: 15,
+    settings: [false, '+'],
   },
   {
     title: 'Crit Cardio',
@@ -100,6 +114,7 @@ const classes = [
     time: '9:00 AM - 10:00 AM',
     instructor: 'Aaron Chapman',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Wheel Pose Full Posture',
@@ -107,6 +122,7 @@ const classes = [
     time: '7:00 AM - 8:30 AM',
     instructor: 'Donna Wilson',
     capacity: 15,
+    settings: [false, '+'],
   },
   {
     title: "Playful Dancer's Flow",
@@ -114,6 +130,7 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Donna Wilson',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Zumba Dance',
@@ -121,6 +138,7 @@ const classes = [
     time: '5:00 PM - 7:00 PM',
     instructor: 'Donna Wilson',
     capacity: 20,
+    settings: [false, '+'],
   },
   {
     title: 'Cardio Blast',
@@ -128,6 +146,7 @@ const classes = [
     time: '5:00 PM - 7:00 PM',
     instructor: 'Randy Porter',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Pilates Reformer',
@@ -135,6 +154,7 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Randy Porter',
     capacity: 10,
+    settings: [false, '+'],
   },
   {
     title: 'Supple Spine and Shoulders',
@@ -142,6 +162,7 @@ const classes = [
     time: '6:30 AM - 8:00 AM',
     instructor: 'Randy Porter',
     capacity: 15,
+    settings: [false, '+'],
   },
   {
     title: 'Yoga for Divas',
@@ -149,6 +170,7 @@ const classes = [
     time: '9:00 AM - 11:00 AM',
     instructor: 'Donna Wilson',
     capacity: 20,
+    settings: [false, '+'],
   },
   {
     title: 'Virtual Cycle',
@@ -156,25 +178,90 @@ const classes = [
     time: '8:00 AM - 9:00 AM',
     instructor: 'Randy Porter',
     capacity: 20,
+    settings: [false, '+'],
   },
 ]
 
-function render() {
-  for (let i = 0; i < classes.length; i++) {
-    tableElement.insertAdjacentHTML('beforeend', getTemplate(classes[i], i))
+removeBtn.onclick = function () {
+  const classesFilter = classes.filter(
+    (item) => item.settings[0] === true,
+  ).length
+
+  function removeBtnForEach(enabled, symb) {
+    classes.forEach((item) => {
+      item.settings[0] = enabled
+      item.settings[1] = symb
+      removeBtn.textContent = symb
+    })
   }
+
+  if (classesFilter > classes.length / 2) {
+    removeBtnForEach(false, '+')
+  } else {
+    removeBtnForEach(true, '-')
+  }
+  render()
+}
+
+function bgColor(index) {
+  if (index % 2 === 0 && !classes[index].settings[0]) {
+    return '#f8f7ff'
+  } else if (classes[index].settings[0]) {
+    return 'rgb(252, 249, 82)'
+  }
+  return 'white'
+}
+
+function render() {
+  tableElement.innerHTML = ''
+  for (let i = 0; i < classes.length; i++) {
+    tableElement.insertAdjacentHTML(
+      'beforeend',
+      getTemplate(classes[i], i, classes[i].settings[1]),
+    )
+  }
+}
+
+tableElement.onclick = function (event) {
+  const index = parseInt(event.target.dataset.index)
+  const type = event.target.dataset.type
+
+  const classesFilter = classes.filter(
+    (item) => item.settings[0] === true,
+  ).length
+
+  if (type === 'btn' && classes[index].settings[0]) {
+    classes[index].settings[0] = false
+    classes[index].settings[1] = '+'
+  } else if (type === 'btn' && !classes[index].settings[0]) {
+    classes[index].settings[0] = true
+    classes[index].settings[1] = '-'
+  }
+
+  // if (classesFilter > classes.length / 2) {
+  //   removeBtn.textContent = '-'
+  // } else {
+  //   removeBtn.textContent = '+'
+  // }
+
+  classesFilter > classes.length / 2
+    ? (removeBtn.textContent = '-')
+    : (removeBtn.textContent = '+')
+
+  render()
 }
 
 render()
 
-function getTemplate(tr, index) {
+function getTemplate(tr, index, symb) {
   return `
-    <tr data-index="${index}" style="background-color:${index % 2 === 0 ? '#f8f7ff' : 'white'}">
+    <tr data-index="${index}" style="background-color:${bgColor(index)}">
       <td>${tr.title}</td>
       <td>${tr.category}</td>
       <td>${tr.time}</td>
       <td>${tr.instructor}</td>
       <td>${tr.capacity}</td>
+      <td><button data-index="${index}" data-type="btn">${symb}</button></td>
     </tr>
   `
 }
